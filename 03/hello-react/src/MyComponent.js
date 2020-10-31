e583248fd0e713cc38b00ea240dbd4e7443192a2
kororo11 <kororo11@naver.com>
2020-10-31 22:08:30 +0900

    비구조화 할당 문법을 함수 파라미터에 사용

--- 03/hello-react/src/MyComponent.js
+++ 03/hello-react/src/MyComponent.js
@@ -1,8 +1,7 @@
 import React from 'react';
 
 // 비구조화 할당 문법을 통해 props 내부 값 추출하기
-const MyComponent = props => {
-	const { name, children } = props;
+const MyComponent = ({ name, children }) => {
 	return (
 		<div>
 			안녕하세요, 제 이름은 {name} 입니다. <br />
