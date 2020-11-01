826bfb79a7737575a1c6e015378d5e8f1687dc01
kororo11 <kororo11@naver.com>
2020-11-01 22:50:21 +0900

    클래스형 컴포넌트의 state

--- 03/hello-react/src/App.js
+++ 03/hello-react/src/App.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import MyComponent from './MyComponent';
+import Counter from './Counter';
 
 const App = () => {
-	return (
-		<MyComponent name="React" favoriteNumber={1}>
-			리엑트
-		</MyComponent>
-	);
+	return <Counter />;
 };
 
 export default App;
