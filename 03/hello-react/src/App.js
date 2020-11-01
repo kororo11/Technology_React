68f1653bffb6eb9e083dde7491dd311fd90476bf
kororo11 <kororo11@naver.com>
2020-11-01 23:05:51 +0900

    클래스형 컴포넌트의 state : Counter 예제

--- 03/hello-react/src/App.js
+++ 03/hello-react/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Counter from './Counter';
-
 const App = () => {
 	return <Counter />;
 };
--- 03/hello-react/src/Counter.js
+++ 03/hello-react/src/Counter.js
@@ -10,14 +10,11 @@ class Counter extends Component {
 
 	render() {
 		const { number } = this.state;
-
 		return (
 			<div>
 				<h1>{number}</h1>
 				<button
-					// onClick 을 통해 버튼이 클릭되었을 때 호출 할 함수를 지정합니다.
 					onClick={() => {
-						// this.setState를 사용하여 state에 새로운 값을 넣을 수 있습니다.
 						this.setState({ number: number + 1 });
 					}}
 				>
