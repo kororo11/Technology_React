e1ac1e821a52027a60e52cff54813f619408bf37
kororo11 <kororo11@naver.com>
2020-11-02 18:06:39 +0900

    이벤트 핸들링: state에 input 값 담기

---
+++ 03/hello-react/src/EventPractice.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class EventPractice extends Component {
+	state = {
+		message: '',
+	};
+
+	render() {
+		return (
+			<div>
+				<h1>이벤트 연습</h1>
+				<input
+					type="text"
+					name="message"
+					placeholder="아무 글이나 적으세요"
+					value={this.state.message}
+					onChange={e => {
+						this.setState({
+							message: e.target.value,
+						});
+					}}
+				/>
+			</div>
+		);
+	}
+}
+
+export default EventPractice;
