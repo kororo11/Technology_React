43da2f965e11bf8e795c521305beea1c152bbf2e
kororo11 <kororo11@naver.com>
2020-11-03 23:36:03 +0900

    데이터 추가 기능 구현하기

--- 06/hello-react/src/IterationSample.js
+++ 06/hello-react/src/IterationSample.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const IterationSample = () => {
-	const names = ['봄', '여름', '가음', '겨울'];
-	const nameList = names.map((name, index) => <li key={index}>{name}</li>);
+	const [names, setNames] = useState([
+		{ id: 1, text: '봄' },
+		{ id: 2, text: '여름' },
+		{ id: 3, text: '가을' },
+		{ id: 4, text: '겨울' },
+	]);
+	const [inputText, setInputText] = useState('');
+	const [nextId, setNextId] = useState(5); // 새로운 항목을 추가할 때 사용할 id
+
+	const onChage = e => setInputText(e.target.value);
+	const onClick = () => {
+		const nextNames = names.concat({
+			id: nextId,
+			text: inputText,
+		});
+		setNextId(nextId + 1);
+		setNames(nextNames);
+		setInputText('');
+	};
+
+	const nameList = names.map(name => <li key={name.id}>{name.text}</li>);
 
 	return (
 		<div>
+			<input type="text" value={inputText} onChange={onChage} />
+			<button onClick={onClick}>추가</button>
 			<ul>{nameList}</ul>
 		</div>
 	);
