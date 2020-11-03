6c83e0e241fbba39a08e344af21138706bbfd942
kororo11 <kororo11@naver.com>
2020-11-03 20:10:17 +0900

    map 사용하기

--- 06/hello-react/src/App.js
+++ 06/hello-react/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import IterationSample from './IterationSample';
 
 function App() {
 	return (
 		<div>
 			<h1>6장 컴포넌트 반복</h1>
+			<IterationSample />
 		</div>
 	);
 }
