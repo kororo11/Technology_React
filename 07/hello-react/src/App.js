a6c2491ad3978091e7b895b39e5064e1a13a2258
kororo11 <kororo11@naver.com>
2020-11-05 17:15:01 +0900

    7장 라이프 사이클

---
+++ 07/hello-react/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import LifeCycleSample from './LifeCycleSample';
+
+function getRandomColor() {
+	return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+class App extends Component {
+	state = {
+		color: '#000000',
+	};
+
+	handleClick = () => {
+		this.setState({
+			color: getRandomColor(),
+		});
+	};
+
+	render() {
+		return (
+			<div>
+				<h2>7장 컴포넌트의 라이프사이클 메서드</h2>
+				<button onClick={this.handleClick}>랜덤색상</button>
+				<LifeCycleSample color={this.state.color} />
+			</div>
+		);
+	}
+}
+export default App;
