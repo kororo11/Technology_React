17b2f2543f5be16d05920df00af7b6be1b9fa054
kororo11 <kororo11@naver.com>
2020-11-05 18:13:40 +0900

    뒷정리하기
    useEffect는 기본적으로 렌더링되고 난 직후마다 실행되며, 두번째 파라미터 배열에 무엇을 넣는지에 따라 실행되는 조건이 달라집니다.
    컴포넌트가 언마운트되기 전이나 업데이트되기 직전에 어떠한 작업을 수행하고 싶다면 useEffect에서 뒷정리(cleanup) 함수를 반환해 주어야 합니다.

--- 08/hooks-tutorial/src/App.js
+++ 08/hooks-tutorial/src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Info from './Info';
 
-function App() {
+const App = () => {
+	const [visible, setVisible] = useState(false);
+
 	return (
 		<div>
 			<h2>8장. Hooks</h2>
-			<Info />
+			<button
+				onClick={() => {
+					setVisible(!visible);
+				}}
+			>
+				{visible ? '숨기기' : '보이기'}
+			</button>
+			<hr />
+			{visible && <Info />}
 		</div>
 	);
-}
+};
 
 export default App;
--- 08/hooks-tutorial/src/Info.js
+++ 08/hooks-tutorial/src/Info.js
@@ -2,17 +2,22 @@ import React, { useState, useEffect } from 'react';
 
 const Info = () => {
 	const [name, setName] = useState('');
-	const [nickName, setNickName] = useState('');
+	const [nickName, setNickname] = useState('');
 
 	useEffect(() => {
+		console.log('useEffect');
 		console.log(name);
-	}, [name]);
+		return () => {
+			console.log('cleanup');
+			console.log(name);
+		};
+	});
+
 	const onChangeName = e => {
 		setName(e.target.value);
 	};
-
 	const onChangeNickName = e => {
-		setNickName(e.target.value);
+		setNickname(e.target.value);
 	};
 
 	return (
@@ -27,12 +32,16 @@ const Info = () => {
 			</div>
 			<div>
 				<div>
-					<b>이름: </b>
-					{name}
+					<div>
+						<b>이름 : </b>
+						{name}
+					</div>
 				</div>
 				<div>
-					<b>닉네임: </b>
-					{nickName}
+					<div>
+						<b>닉네임 : </b>
+						{nickName}
+					</div>
 				</div>
 			</div>
 		</div>
