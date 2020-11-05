022652588cc8baea96e6bb7c20436ecf56c6740c
kororo11 <kororo11@naver.com>
2020-11-05 23:46:20 +0900

    useReducer 로 인풋 상태 관리하기

--- 08/hooks-tutorial/src/Info.js
+++ 08/hooks-tutorial/src/Info.js
@@ -1,47 +1,45 @@
-import React, { useState, useEffect } from 'react';
+import React, { useReducer } from 'react';
+
+function reducer(state, action) {
+	return {
+		...state,
+		[action.name]: action.value,
+	};
+}
 
 const Info = () => {
-	const [name, setName] = useState('');
-	const [nickName, setNickname] = useState('');
-
-	useEffect(() => {
-		console.log('useEffect');
-		console.log(name);
-		return () => {
-			console.log('cleanup');
-			console.log(name);
-		};
+	const [state, dispath] = useReducer(reducer, {
+		name: '',
+		nickname: '',
 	});
 
-	const onChangeName = e => {
-		setName(e.target.value);
-	};
-	const onChangeNickName = e => {
-		setNickname(e.target.value);
+	const { name, nickname } = state;
+	const onChange = e => {
+		dispath(e.target);
 	};
 
 	return (
 		<div>
 			<div>
-				<input type="text" value={name} onChange={onChangeName} />
 				<input
 					type="text"
-					value={nickName}
-					onChange={onChangeNickName}
+					name="name"
+					value={name}
+					onChange={onChange}
+				/>
+				<input
+					type="text"
+					name="nickname"
+					value={nickname}
+					onChange={onChange}
 				/>
 			</div>
 			<div>
 				<div>
-					<div>
-						<b>이름 : </b>
-						{name}
-					</div>
+					<b>이름 : </b> {name}
 				</div>
 				<div>
-					<div>
-						<b>닉네임 : </b>
-						{nickName}
-					</div>
+					<b>닉네임 : </b> {nickname}
 				</div>
 			</div>
 		</div>
